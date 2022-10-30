37e00b3004bb01f7fc417e4d58ed817ac968ff1a
Raptor-Dev97 <57743677+Raptor-Dev97@users.noreply.github.com>
2022-10-30 15:07:11 +0530

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -5,9 +5,9 @@ public static void main(String[] args)
 {
 Scanner scanner = new Scanner(System.in);
 System.out.println("Please provide number of rows to print... ");
-int myrows = scanner.nextInt();
+int rows = scanner.nextInt();
 System.out.println("\nThe star pattern is... ");
-for (int m = 1; m <= myrows; m++)
+for (int m = 1; m <= rows; m++)
 {
 for (int n = 1; n <= m; n++)
 {
